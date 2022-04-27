73932b41fb35cc7759c718bdee929a9b957a3cc9
Leonhart Setiawan <leonhart.21@ichat.sp.edu.sg>
2022-04-27 13:47:41 +0800

    Proper Lab2 code

--- Lab2.py
+++ Lab2.py
@@ -1,4 +1,38 @@
-def main():
-    print("why the fk does python use indentation instead of semicolons :(")
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+    num = get_user_input()
+    avg = calc_average_temp(num)
+    minmax = calc_minmax_temp(num)
+    print("Average temp is : ", end='')
+    print(avg, "C")
+    print("minimun and maximumtemp is ", minmax)
 
-main()
+def get_user_input():
+    inp = input("Enter : ")
+    x = inp.split(",")
+    return x
+
+def calc_average_temp(num):
+    elements = len(num)
+    total = 0
+    for x in num:
+        num1 = float(x)
+        total = total + num1
+    avg = total / elements
+    return avg
+
+def calc_minmax_temp(num):
+    elements = len(num)
+    total = 0
+    min = 1000000
+    max = 0
+    for x in num:
+        num1 = float(x)
+        if(num1 < min):
+            min = num1
+
+        elif(num1 > max):
+            max = num1
+
+    return min, max
+display_main_menu()
